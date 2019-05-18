a178cc2a48ce88a64f9b9edb0548aa4a94034474
Jordan <fieldcom@mail.com>
2019-05-18 18:01:27 +0000

    Replace userProfilePage.js

--- src/Pages/userProfilePage.js
+++ src/Pages/userProfilePage.js
@@ -1,53 +1,66 @@
-import React from 'react'
-import API from '../api';
-import { Card, Icon, Image, Label } from 'semantic-ui-react';
+import React, { useState, useEffect } from "react";
+import API from "../api";
+import {
+  Card,
+  Divider,
+  Grid,
+  Row,
+  Segment,
+  Icon,
+  Image,
+  Label,
+  LabelDetail,
+  GridRow
+} from "semantic-ui-react";
 
-
-
-class UserProfile extends React.Component {
-    constructor(props){
-      super(props);
-      this.state = {
-        userID: 1
-      }
-    }
+function UserSmallCard(props) {
+  const [userId, setUserId] = useState(1);
   
-    componentDidMount(){
-      API.get(`users/${this.state.userID}`)
-        .then(result => {
-          console.log(result);
-          console.log(result.data)
-        })
-    }
-    
-    render() {
-      return (
-          <div>
-            <Image src='../images/square-image.png' size='medium' circular /> 
-            <Image src='../images/square-image.png' fluid />
-            <Label>
-                Blender 3D
-            </Label>
+  useEffect(() => {
+    API.get(`users/${userID}`).then(result => {
+      console.log(result);
+      console.log(result.data);
+    });
+  }, [userId]);
 
-        <Card raised>
-          <Card.Content>
-            <Image floated='right' size='mini' src='https://react.semantic-ui.com/images/avatar/large/steve.jpg' />
-            <Card.Header>deranjer</Card.Header>
-            <Card.Meta>Looking to Collaborate</Card.Meta>
-            <Card.Description>
-              Experienced 3D Modeler in Blender. Also Experienced in Substance Painter
-            </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            <a>
-                <Icon name='tags' />
-                3D Modeling, Texturing, golang, deployment, UE4, Unity
-            </a>
-          </Card.Content>
-        </Card>
-        </div>
-        );
-      }
-  }
-  
-  export default UserProfile
+  return (
+    <Grid textAlign="center">
+      <Card padded raised style={{ height: "250px" }}>
+        <Card.Content textAlign="left">
+          <Image
+            floated="center"
+            size="mini"
+            src="https://react.semantic-ui.com/images/avatar/large/steve.jpg"
+          />
+          <Card.Header>deranjer</Card.Header>
+          <Card.Meta>Looking to Collaborate</Card.Meta>
+          <Card.Description>
+            Experienced 3D Modeler in Blender. Experienced in Substance Painter
+          </Card.Description>
+          <Divider horizontal>Skills</Divider>
+          <Grid textAlign="center" columns="equal" style={{ height: "100px" }}>
+            <Grid.Row style={{ height: "0px" }}>
+              <Label>3D Modeling</Label>
+              <Label>Texturing</Label>
+              <Label>golang</Label>
+            </Grid.Row>
+            <Grid.Row style={{ height: "0px" }}>
+              <Label>deployment</Label>
+              <Label>UE4</Label>
+              <Label>golang</Label>
+              <Label>UE4</Label>
+            </Grid.Row>
+            <Grid.Row style={{ height: "0px" }}>
+              <Label>deployment</Label>
+              <Label>UE4</Label>
+              <Label>UE4</Label>
+              <Label>golang</Label>
+            </Grid.Row>
+          </Grid>
+        </Card.Content>
+      </Card>
+    </Grid>
+  );
+}
+
+export default UserSmallCard;
