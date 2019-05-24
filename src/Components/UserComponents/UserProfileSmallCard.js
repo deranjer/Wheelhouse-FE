e80ab774d367a6048ea2bac0b9404654a13188f1
deranjer <deranjer@gmail.com>
2019-05-23 22:02:26 -0400

    Adding some quick commit to userprofilesmallcard

--- src/Components/UserComponents/UserProfileSmallCard.js
+++ src/Components/UserComponents/UserProfileSmallCard.js
@@ -9,11 +9,13 @@ import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Divider from '@material-ui/core/Divider';
 import Chip from '@material-ui/core/Chip';
+import { IconButton } from "@material-ui/core";
 
 
-function UserSmallCard(props) {
+const UserSmallCard = (props) => {
   const [userId, setUserId] = useState(1);
   
   useEffect(() => {
@@ -23,6 +25,9 @@ function UserSmallCard(props) {
     });
   }, [userId]);
 
+  const handleClick = (event) => {
+
+  }
   
   return (
     <Grid container>
@@ -34,6 +39,13 @@ function UserSmallCard(props) {
                 title="deranjer"
                 subheader="Looking to Collaborate"
                 avatar={<Avatar aria-label="Recipe">D</Avatar>}
+                action={
+                  <IconButton>
+                    <MoreVertIcon />
+                    
+                  </IconButton>
+                }
+              
               />
               <CardContent>
                 <Typography>
@@ -42,8 +54,17 @@ function UserSmallCard(props) {
                   Looking for a challange.
                   </Typography>
                 <Typography align="center" variant="h6">Skills</Typography>
-                <Divider variant="fullWidth"/>
-                <Chip label="3d Modeling"/>
+                <Divider variant="fullWidth" style={{ margin: "5px"}}/>
+                <Grid container justify="center">
+                  <Chip label="3d Modeling" style={{ margin: "5px"}}/>
+                  <Chip label="Texturing" style={{ margin: "5px"}}/>
+                  <Chip label="golang" style={{ margin: "5px"}}/>
+                </Grid>
+                <Grid container justify="center">
+                  <Chip label="UE4" style={{ margin: "5px"}}/>
+                  <Chip label="Texturing" style={{ margin: "5px"}}/>
+                  <Chip label="3d Modeling" style={{ margin: "5px"}}/>
+                </Grid>
               </CardContent> 
 
 
