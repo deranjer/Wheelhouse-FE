c75eb4d84d9add37921b1b216c45216736c82d0f
deranjer <deranjer@gmail.com>
2019-05-25 21:13:48 -0400

    Making changes to the user profile page.

--- src/Components/Pages/userProfilePage.js
+++ src/Components/Pages/userProfilePage.js
@@ -1,11 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import PropTyes from 'prop-types';
 import API from '../../api';
-import {Menu, MenuItem,  Avatar, Typography, Grid, Divider, Chip, Paper} from "@material-ui/core";
+import {Menu, MenuItem,  Avatar, Typography, Grid, Divider, Chip, Paper, Card, CardContent, CardDescription} from "@material-ui/core";
+import {IconButton, Button} from "@material-ui/core";
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 var imageOverLapStyle = {
   position: 'absolute',
-  top: '100px'
+  top: '100px',
+}
+
+var profileImageStyle = {
+  width: '300px',
+  height: '300px',
+  borderRadius: '50%'
+}
+
+var profileBGImageStyle = {
+  width: '100%',
+  height: '100%',
+  borderRadius: '25px'
 }
 
 const UserProfile = (props) => {
@@ -21,28 +35,57 @@ const UserProfile = (props) => {
 
 
   return (
-    <div style={{flexGrow: 1}}>
-      <Grid container spacing={1}>
-        <Grid item xs={2}>{/*  Right side bar */}
-          <Paper >xs=2</Paper>
+    <div>
+      <Grid container spacing={1 }>
+        <Grid item xs={2}>{/*  Left Side Top Bar */}
+          <Paper style={{height: '100%'}}>Left Side Top Bar</Paper>
         </Grid>
         <Grid item xs={8}>{/*  Header Image */}
-          <Paper >xs=8</Paper>
+          <img style={profileBGImageStyle} src='https://www.van-tramp.com/wp/wp-content/uploads/2013/07/IMG_5831.jpg'/>
+          <div style={imageOverLapStyle}><img style={profileImageStyle} src='http://www.mojosolitservices.com/wp-content/uploads/avatar-1-300x300.png'/></div>
         </Grid>
-        <Grid item xs={2}>{/*  Left side bar */}
-          <Paper >xs=2</Paper>
+        <Grid item xs={2}>{/*  Right Side Top Bar */}
+          <Paper style={{height: '100%'}}>Right Side Top Bar</Paper>
         </Grid>
-        <Grid item xs={2}>{/*  Right side bar */}
-          <Paper >xs=2</Paper>
+        <Grid item xs={2}>{/*  Left Side Bottom Bar */}
+          <Paper style={{height: '100%'}} >Left Side Bottom Bar</Paper>
         </Grid>
         <Grid item xs={2}>{/*  Left Menu/Column */}
-          <Paper >xs=2</Paper>
+          <Grid container spacing={1}>
+            <Grid item>           
+              <Typography  variant='h4'>Username</Typography>
+            </Grid>
+            <Grid item>
+              <IconButton style={{float: 'right'}}>
+                <MoreVertIcon />                    
+              </IconButton>
+            </Grid>
+          </Grid> 
+          <Card>
+            <CardContent>I am an experienced user with Blender and Substance Painter.  I have a one released game created with Unity.  I also have a lot of experience with UE4 Blueprints, materials and Niagara System.  Looking for a unique game to create.</CardContent>
+          </Card>
+          <Typography variant='h4'>Messages</Typography>
+
         </Grid>
         <Grid item xs={6}>{/*  Main Page Content */}
-          <Paper >xs=6</Paper>
+          <Grid container justify="flex-end">
+            <Button style={{ margin: "5px"}} variant="contained" color="primary">Message</Button>
+            <Button style={{ margin: "5px"}} variant="contained" color="primary">Follow</Button>
+          </Grid>
+          
+          
+          
+          <Grid container justify="center">
+            <Chip label="3d Modeling" style={{ margin: "5px"}}/>
+            <Chip label="Texturing" style={{ margin: "5px"}}/>
+            <Chip label="golang" style={{ margin: "5px"}}/>
+            <Chip label="UE4" style={{ margin: "5px"}}/>
+            <Chip label="Texturing" style={{ margin: "5px"}}/>
+            <Chip label="3d Modeling" style={{ margin: "5px"}}/>
+          </Grid>
         </Grid>
-        <Grid item xs={2}>{/*  Left side bar */}
-          <Paper >xs=2</Paper>
+        <Grid item xs={2}>{/*  Right side Bottom bar */}
+          <Paper style={{height: '100%'}}>Right side Bottom bar</Paper>
         </Grid>
       </Grid>
     </div>
