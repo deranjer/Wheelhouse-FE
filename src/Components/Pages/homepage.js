337fcafba292b4c6b07f4c0b1ff9dd2f110db497
deranjer <deranjer@gmail.com>
2019-06-12 22:16:42 -0400

    Fixing homepage grid layout.

--- src/Components/Pages/homepage.js
+++ src/Components/Pages/homepage.js
@@ -8,40 +8,25 @@ import ProjectSmallCard from "../ProjectComponents/ProjectSmallCard";
 const HomePage = props => {
   return (
     <Grid container spacing={2}>
-      <Grid item xs={12}>
-        {/*  Top Menu Bar */}
-        {/* <TopNavBar changeTheme={props.changeTheme} themeIcon={props.themeIcon} /> */}
-        {/* shouldn't be here! because we'd have to put in every page. moved to App.js like the "bottom" one */}
-      </Grid>
-      {/* End Top Menu Bar */}
-      {/* Separation between top nav bar and main text */}
       <Grid item xs={12} />
       <Grid container justify="center">
         {/* Main Page Content */}
-        <Grid item xs={2} />
-        {/* Left Side Padding, could just float the text right and perhaps get the same effect */}
-        <Grid item xs={5}>
-          <Typography variant="h3">Welcome to Wheelhouse</Typography>
+        <Grid item xs={8} lg={4} xl={4}>
+        <Typography variant="h3">Welcome to Wheelhouse</Typography>
         </Grid>
-        <Grid item xs={5}>
+        <Grid item xs={4} lg={4} xl={4}>
           <Button variant="contained" color="primary">JOIN!</Button>
         </Grid>
       </Grid>
       <Grid container justify="center">
-        <Grid item xs={2} />
-        {/* Left Side Padding, could just float the text right and perhaps get the same effect */}
-        <Grid item xs={8}>
+        <Grid item xs={12} lg={8} xl={8}>
           <Typography variant="h6">
             A team building community for developers and designers
           </Typography>
         </Grid>
-        <Grid item xs={2} />
-        {/* Right Side Padding */}
       </Grid>
       <Grid container justify="center">
-        <Grid item xs={2} />
-        {/* Left Side Padding */}
-        <Grid item xs={8}>
+        <Grid item xs={12} lg={8} xl={8}>
           <Paper>
             <Grid container>
               <ProjectSmallCard />
@@ -52,8 +37,6 @@ const HomePage = props => {
             </Grid>
           </Paper>
         </Grid>
-        <Grid item xs={2} />
-        {/* Right Side Padding */}
       </Grid>
       {/* Footer */}
       <Grid item xs={12} />
