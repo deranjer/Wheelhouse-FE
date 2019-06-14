21dbf3726864b637225f26d09bc70c4b278e2dc4
Muntadhar Haydar <sparkist97@outlook.com>
2019-06-14 06:31:28 +0300

    new userProfilePage and its styles

--- src/Components/Pages/userProfilePage.js
+++ src/Components/Pages/userProfilePage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import PropTyes from "prop-types";
 import API from "../../api";
 import {
@@ -12,32 +12,22 @@ import {
   Paper,
   Card,
   CardContent,
-  CardDescription
+  CardDescription,
+  Hidden,
 } from "@material-ui/core";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import { makeStyles } from "@material-ui/styles";
 import { IconButton, Button } from "@material-ui/core";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import UserPortfolioCard from "../UserComponents/UserPortfolioCard";
 import ProjectSmallCard from "../ProjectComponents/ProjectSmallCard";
 
-var imageOverLapStyle = {
-  position: "absolute",
-  top: "150px"
-};
-
-var profileImageStyle = {
-  width: "300px",
-  height: "300px",
-  borderRadius: "50%"
-};
-
-var profileBGImageStyle = {
-  width: "100%",
-  height: "100%",
-  borderRadius: "25px"
-};
+import getProfilePageClasses from "../../Styles/profilePageStyles";
 
 const UserProfile = props => {
-  const [userId, setUser] = useState(0);
+  const [user, setUser] = useState(0);
 
   async function getUser() {
     try {
@@ -54,119 +44,67 @@ const UserProfile = props => {
     getUser();
   }, []);
 
+  const classes = getProfilePageClasses(props.appTheme);
+
   return (
-    <React.Fragment>
+    <div className={classes.RootElClasses.root}>
       <Grid container justify="center">
-        <Grid item xs={12} lg={8} xl={8}>
-          {/*  Header Image */}
-          <img
-            style={profileBGImageStyle}
-            src="https://www.van-tramp.com/wp/wp-content/uploads/2013/07/IMG_5831.jpg"
-          />
-          <div style={imageOverLapStyle}>
+        <Grid item xs={12} md={10} lg={8} container>
+          <Grid item xs={12} className={classes.ProfileBGClasses.root}>
             <img
-              style={profileImageStyle}
-              src="http://www.mojosolitservices.com/wp-content/uploads/avatar-1-300x300.png"
+              src="https://www.van-tramp.com/wp/wp-content/uploads/2013/07/IMG_5831.jpg"
+              alt="profile background"
+              className="imgHero"
             />
-          </div>
-        </Grid>
-        <Grid container justify="center" alignContent="space-around">
-          <Grid item xs={2} lg={2} xl={2}>
-            <Grid container>
-              <Typography variant="h4">{userId.username}</Typography>
-              <Grid item>
-                <IconButton style={{ float: "right" }}>
-                  <MoreVertIcon />
-                </IconButton>
-              </Grid>
-            </Grid>
           </Grid>
-          <Grid item xs={6} lg={4} xl={4}>
-            <Typography variant="h6">
-              Here is my tagline that is under 240 that shows up on my
-              profilecard
-            </Typography>
-          </Grid>
-          <Grid item xs={4} lg={2} xl={2}>
-            <Grid container justify="flex-end">
-              <Button
-                style={{ margin: "4px" }}
-                variant="contained"
-                color="primary"
-              >
-                Message
-              </Button>
-              <Button
-                style={{ margin: "4px" }}
-                variant="contained"
-                color="primary"
-              >
-                Follow
-              </Button>
-            </Grid>
+          <Grid item xs={12} className={classes.ProfileImageClasses.root}>
+            <img
+              src="http://www.mojosolitservices.com/wp-content/uploads/avatar-1-300x300.png"
+              alt="profile"
+              className="imgPfp"
+            />
+            <div>
+              <Typography variant="h6">{user.username}</Typography>
+              <Typography variant="subtitle2">{user.full_name}</Typography>
+            </div>
           </Grid>
         </Grid>
       </Grid>
-      <Grid container justify="center" spacing={1}>
-        <Grid item xs={6} lg={2} xl={2}>
+      <Grid container justify="center" className={classes.TaglineClasses.root}>
+        <Grid item xs={12} md={5} lg={4}>
+          <Typography variant="subtitle1">
+            Here is my tagline that is under 240 that shows up on my profilecard
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={5} lg={4} container justify="flex-end">
+          <Button variant="contained" type="submit" color="primary">
+            Follow
+          </Button>
+          <Button variant="contained" type="submit" color="primary">
+            Message
+          </Button>
+        </Grid>
+      </Grid>
+      <Grid container justify="center" className={classes.AboutClasses.root}>
+        <Grid item xs={12} md={5} lg={4}>
           <Card>
             <CardContent>
-              I am an experienced user with Blender and Substance Painter. I
-              have a one released game created with Unity. I also have a lot of
-              experience with UE4 Blueprints, materials and Niagara System.
-              Looking for a unique game to create.
+              I am an experienced user with Blender and Substance Painter. I have a one released
+              game created with Unity. I also have a lot of experience with UE4 Blueprints,
+              materials and Niagara System. Looking for a unique game to create.
             </CardContent>
           </Card>
         </Grid>
-        <Grid item xs={6} lg={6} xl={6}>
-          <Chip label="Substance Designerrr" style={{ margin: "4px" }} />
-          {/*  20 char max for tags */}
-          <Chip label="Substance Designerrr" style={{ margin: "4px" }} />
-          <Chip label="Substance Designerrr" style={{ margin: "4px" }} />
-          <Chip label="Substance Designerrr" style={{ margin: "4px" }} />
-          <Chip label="Substance Designerrr" style={{ margin: "4px" }} />
-          <Chip label="Substance Designerrr" style={{ margin: "4px" }} />
-          <Chip label="Substance Designerrr" style={{ margin: "4px" }} />
-          <Typography variant="h4" align="center">
-            Portfolio
-          </Typography>
-          <Grid container spacing={1} justify="flex-start">
-            <Grid item>
-              <UserPortfolioCard />
-            </Grid>
-            <Grid item>
-              <UserPortfolioCard />
-            </Grid>
-            <Grid item>
-              <UserPortfolioCard />
-            </Grid>
-            <Grid item>
-              <UserPortfolioCard />
-            </Grid>
-            <Grid item>
-              <UserPortfolioCard />
-            </Grid>
-            <Grid item>
-              <UserPortfolioCard />
-            </Grid>
-          </Grid>
-          <Typography variant="h4" align="center">
-            Projects
-          </Typography>
-          <Grid container spacing={1} justify="flex-start">
-            <Grid item>
-              <ProjectSmallCard />
-            </Grid>
-            <Grid item>
-              <ProjectSmallCard />
-            </Grid>
-            <Grid item>
-              <ProjectSmallCard />
-            </Grid>
-          </Grid>
+        <Grid item xs={12} md={5} lg={4} container justify="space-evenly">
+          <Chip label="Substance Designerrr" />
+          <Chip label="Substance Designerrr" />
+          <Chip label="Substance Designerrr" />
+          <Chip label="Substance Designerrr" />
+          <Chip label="Substance Designerrr" />
+          <Chip label="Substance Designerrr" />
         </Grid>
       </Grid>
-    </React.Fragment>
+    </div>
   );
 };
 
---
+++ src/Styles/profilePageStyles.js
@@ -0,0 +1,105 @@
+import { makeStyles } from "@material-ui/styles";
+
+function getProfilePageClasses(theme) {
+  const profileBGStyles = makeStyles({
+    root: {
+      maxHeight: "280px",
+      "& .imgHero": {
+        height: "100%",
+        width: "100%",
+        borderRadius: "25px",
+        // [theme.breakpoints.up("xs")]: {
+        //   padding: "0 10px",
+        // },
+      },
+    },
+  });
+
+  const profileImageStyles = makeStyles({
+    root: {
+      transform: "translate(1%, -50%)",
+      [theme.breakpoints.up("xs")]: {
+        padding: "0 10px",
+      },
+
+      "& img": {
+        borderRadius: "50%",
+        height: "96px",
+        width: "96px",
+        [theme.breakpoints.up("md")]: {
+          height: "128px",
+          width: "128px",
+        },
+        [theme.breakpoints.up("lg")]: {
+          height: "160px",
+          width: "160px",
+        },
+      },
+
+      "& div": {
+        marginLeft: "1%",
+        display: "inline-block",
+        [theme.breakpoints.up("md")]: {
+          transform: "translateY(-25%)",
+        },
+        [theme.breakpoints.up("lg")]: {
+          transform: "translateY(-50%)",
+        },
+      },
+    },
+  });
+
+  const taglineStyles = makeStyles({
+    root: {
+      marginTop: "-45px!important",
+      [theme.breakpoints.up("md")]: {
+        marginTop: "-65px!important",
+      },
+      [theme.breakpoints.up("xl")]: {
+        marginTop: "-90px!important",
+      },
+      "& .MuiGrid-item > .MuiButtonBase-root": {
+        marginRight: 10,
+        height: 40,
+      },
+      "& .MuiGrid-item.MuiGrid-container": {
+        justifyContent: "flex-end",
+        [theme.breakpoints.down("sm")]: {
+          justifyContent: "flex-start",
+        },
+      },
+    },
+  });
+
+  const aboutStyles = makeStyles({
+    root: {
+      "& .MuiGrid-item": {
+        [theme.breakpoints.down("sm")]: {
+          marginTop: "10px!important",
+        },
+      },
+
+      "& .MuiGrid-item > .MuiChip-root": {
+        margin: "0 4px 4px 0px",
+      },
+    },
+  });
+
+  const rootElStyles = makeStyles({
+    root: {
+      [theme.breakpoints.down("sm")]: {
+        padding: "0 10px",
+      },
+    },
+  });
+
+  const ProfileBGClasses = profileBGStyles();
+  const ProfileImageClasses = profileImageStyles();
+  const AboutClasses = aboutStyles();
+  const RootElClasses = rootElStyles();
+  const TaglineClasses = taglineStyles();
+
+  return { ProfileBGClasses, ProfileImageClasses, AboutClasses, RootElClasses, TaglineClasses };
+}
+
+export default getProfilePageClasses;
