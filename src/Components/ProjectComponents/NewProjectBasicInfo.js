3a92fe52369ee3a014b7ee05ddd63c00bd8dbc46
deranjer <deranjer@gmail.com>
2019-06-24 17:02:20 -0400

    Small changes on new project setup, tracking down an odd bug with imports and exports.

--- src/Components/ProjectComponents/NewProjectBasicInfo.js
+++ src/Components/ProjectComponents/NewProjectBasicInfo.js
@@ -4,33 +4,50 @@ import { DatePicker } from "@material-ui/pickers";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { Typography, Grid, Paper, InputBase } from "@material-ui/core";
 import { IconButton, Button, Fab } from "@material-ui/core";
+import { DropzoneArea } from "material-ui-dropzone";
 
 const NewProjectBasicInfo = props => {
   const [selectedDate, handleDateChange] = useState(new Date());
+  const [backgroundImage, setBackgroundImage] = useState("");
   const [form, setFormInfo] = React.useState({
     projectName: "",
     projectWebPage: "",
     projectPageTwo: "", //secondary web page user can add, maybe a trello or something
     projectTagLine: "", //240 char tagline
-    projectBlurb: "", //1200 char?
-    projectBackgroundImage: "" //image uploader
+    projectBlurb: "" //1200 char?
   });
   return (
     <Grid item xs={12} lg={8} xl={8}>
       <Paper style={{ textAlign: "center", height: "100%" }}>
         <Formik>
           <Form>
-            <Typography component="p">
-              <Typography component="p">Project Name: </Typography>
+            <Typography>
+              <Typography>Project Name: </Typography>
               <Field type="text" name="projectName" />
             </Typography>
-            <Typography component="p">Project Start Date: </Typography>
+            <Typography>Project Tagline (240 char): </Typography>
+            <Field type="text" name="projectTagLine" />
+            <Typography>Project Blurb (600 char): </Typography>
+            <Field component="textarea" name="projectBlurb" />
+            <Typography>Project Background Image: </Typography>
+            <DropzoneArea
+              acceptedFiles={["image/*"]}
+              filesLimit={1}
+              maxFileSize={3000000}
+              onChange={setBackgroundImage}
+            />
+            <Typography>Project Start Date: </Typography>
             <DatePicker
               format="MMM DD, YYYY"
               disablePast
               value={selectedDate}
               onChange={handleDateChange}
             />
+
+            <Typography>Project Web Page (optional): </Typography>
+            <Field type="text" name="projectWebPage" />
+            <Typography>Project Web Page 2 (optional):</Typography>
+            <Field type="text" name="projectName" />
           </Form>
         </Formik>
 
--- src/Components/ProjectComponents/NewProjectTeam.js
+++ src/Components/ProjectComponents/NewProjectTeam.js
@@ -2,19 +2,37 @@ import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { DatePicker } from "@material-ui/pickers";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import { Typography, Grid, Paper, InputBase } from "@material-ui/core";
+import {
+  Typography,
+  Grid,
+  Paper,
+  Input,
+  InputLabel,
+  InputAdornment,
+  TextField
+} from "@material-ui/core";
 import { IconButton, Button, Fab } from "@material-ui/core";
+import Search from "@material-ui/icons/Search";
 
-const NewProjectMileStones = props => {
+const NewProjectTeam = props => {
   return (
     <Grid item xs={12} lg={8} xl={8}>
       <Paper style={{ textAlign: "center", height: "100%" }}>
+        <Typography variant="h4">Send out invites to team members</Typography>
+        <br />
+        <TextField
+          id="searchInput"
+          label="Search for Users"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Search />
+              </InputAdornment>
+            )
+          }}
+        />
         <Formik>
-          <Form>
-            <Typography variant="h4">
-              Send out invites to team members
-            </Typography>
-          </Form>
+          <Form />
         </Formik>
         <Button
           style={{
@@ -41,9 +59,9 @@ const NewProjectMileStones = props => {
   );
 };
 
-NewProjectMileStones.propTypes = {
+NewProjectTeam.propTypes = {
   userID: PropTypes.string
   //TODO: Add More PropTypes
 };
 
-export default NewProjectMileStones;
+export default NewProjectTeam;
